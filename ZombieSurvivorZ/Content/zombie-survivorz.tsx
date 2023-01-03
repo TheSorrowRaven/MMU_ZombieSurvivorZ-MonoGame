74ff720405ad5c5e3bf75b1dd5fe07e1b7adca5b
ewrn <91323579+ewrn@users.noreply.github.com>
2023-01-03 18:49:22 +0800

    map

--- ZombieSurvivorZ/Content/zombie-survivorz.tsx
+++ ZombieSurvivorZ/Content/zombie-survivorz.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="zombie-survivorz" tilewidth="64" tileheight="64" tilecount="5" columns="5">
+<tileset version="1.9" tiledversion="1.9.2" name="zombie-survivorz" tilewidth="64" tileheight="64" tilecount="6" columns="6">
  <image source="tileset_temp.png" width="320" height="64"/>
 </tileset>
