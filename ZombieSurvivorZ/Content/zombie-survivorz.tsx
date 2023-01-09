64a8f25c8886a17d67b55424d95a2755fca8de57
ewrn <91323579+ewrn@users.noreply.github.com>
2023-01-09 16:02:04 +0800

    euwern

--- ZombieSurvivorZ/Content/zombie-survivorz.tsx
+++ ZombieSurvivorZ/Content/zombie-survivorz.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="zombie-survivorz" tilewidth="64" tileheight="64" tilecount="6" columns="6">
- <image source="tileset_temp.png" width="320" height="64"/>
+<tileset version="1.9" tiledversion="1.9.2" name="zombie-survivorz.tsx" tilewidth="64" tileheight="64" tilecount="36" columns="6">
+ <image source="tileset_temp.png" width="384" height="384"/>
 </tileset>
